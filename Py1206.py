7e9c0321c03db8c5219b34573591d71f506db60d
agos <agosnoriega2018@gmail.com>
2023-06-12 15:23:23 -0300

    mi primer commit

---
+++ Py1206.py
@@ -0,0 +1,13 @@
+#Entradas
+turno = int(input('Ingrese el turno 1- Diurno y 2- Nocturno :'))
+horas = int(input('Ingrese la cant de horas trabajadas: '))
+#Procesos
+if turno == 1:
+    #salario = 5234.5 * horas
+    print(5234.5 * horas)
+elif  turno == 2:
+    #salario = 8049.75 * horas
+    print(8049.75 * horas)
+else:
+    print('Introduzca un valor correcto')
+    
